a385382101f6c30abab3de611cc45a1f6c9248cb
himatvarsani-cloudreach <himat.varsani@cloudreach.com>
2022-01-13 10:41:17 +0000

    Fix code and amended the VPC IP

--- vpc/route_table.tf
+++ vpc/route_table.tf
@@ -22,14 +22,20 @@ resource "aws_route_table" "public_subnet_rt" {
             }
 }
 
-#ASSOCIATION TO SUBNET PUBLIC A
+# ROUTE TABLE ASSOCIATION TO SUBNET PUBLIC A
 resource "aws_route_table_association" "igw_for_public_a" {
   subnet_id      = aws_subnet.public_a.id
   route_table_id = aws_route_table.public_to_internet_rt.id
 }
 
-#ASSOCIATION TO SUBNET PRIVATE A
-# resource "aws_route_table_association" "private_rt_for_public_a" {
-#   subnet_id      = aws_subnet.private_a.id
-#   route_table_id = aws_route_table.public_subnet_rt.id
-# }
+# RT ASSOCIATION TO SUBNET PRIVATE A FOR LOGSTASH AND ELASTICSEARCH
+resource "aws_route_table_association" "private_rt_for_L_E" {
+    subnet_id      = aws_subnet.private_a.id
+    route_table_id = aws_route_table.public_subnet_rt.id
+}
+
+# RT ASSOCIATION TO SUBNET PRIVATE B FOR TESTING
+resource "aws_route_table_association" "private_rt_for_test" {
+    subnet_id      = aws_subnet.private_b.id
+    route_table_id = aws_route_table.public_subnet_rt.id
+}
