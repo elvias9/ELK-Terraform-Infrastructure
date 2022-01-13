151c6f2d6d15e16f5f1672cb8066c988b23d79cf
elvia-si <elvia.sicuro@cloudreach.com>
2022-01-13 15:45:27 +0100

    fixed network module

--- elasticsearchEC2.tf
+++ elasticsearchEC2.tf
@@ -1,13 +1,13 @@
 module "network" {
   source = "/vpc"
   name = "my_vpc"
-  vpc_cidr         = "10.0.0.0/16"
+  vpc_cidr         = "10.10.0.0/16"
   region           = "eu-west-1"
   vpc_name         = "ELK-Stack-VPC"
   internet_gw_name = "team1-new-INT-GW"
-  public_cidr_a    = "10.10.0.0/24"
+  public_cidr_a    = "10.10.1.0/24"
   private_cidr_a = "10.10.2.0/24"
-  private_cidr_b = "10.10.4.0/24"
+  private_cidr_b = "10.10.3.0/24"
   enable_nat_gateway  = true
   single_nat_gateway = true
 }
