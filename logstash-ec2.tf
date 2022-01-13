daf48c6f4b87596dbe07ff0b8c586c0e342d92fc
courtney-bawuah <courtney.bawuah@cloudreach.com>
2022-01-13 15:38:09 +0000

    logstash ec2 amendment

--- logstash-ec2.tf
+++ logstash-ec2.tf
@@ -1,9 +1,22 @@
+module "network" {
+  source = "/vpc"
+  
+  vpc_cidr         = "10.10.0.0/16"
+  region           = "eu-west-1"
+  vpc_name         = "ELK-Stack-VPC"
+  internet_gw_name = "team1-new-INT-GW"
+  public_cidr_a    = "10.10.1.0/24"
+  private_cidr_a = "10.10.2.0/24"
+  private_cidr_b = "10.10.3.0/24"
+}
+
 resource "aws_security_group" "my_app_sg" {
   name        = "my_app_sg"
   description = "Allow access to my Server"
-  vpc_id      = 
+  vpc_id      = aws_vpc.main_vpc.id
 
-    # INBOUND RULES
+
+# INBOUND RULES
   ingress {
     description      = "SSH from my mac"
     from_port        = 22
@@ -24,20 +37,11 @@ resource "aws_security_group" "my_app_sg" {
   }
 }
 
-data "aws_ami" "my_aws_ami" {
-    owners = ["137112412989"]
-    most_recent = true
-    filter {
-        name = "name"
-        values = [ "amzn2-ami-kernel-*" ]
-    }
-}
-
 #EC2
 resource "aws_instance" "logstash" {
-    ami = data.aws_ami.my_aws_ami.id
+    ami = dami-07d8796a2b0f8d29c
     instance_type = var.instance_type
-    key_name = var.keypair_name
+    key_name = Team1KeyPair
     subnet_id = aws_subnet.private_a.id
     vpc_security_groups_ids = [ aws_security_group.my_app_sg.id ]
 }
