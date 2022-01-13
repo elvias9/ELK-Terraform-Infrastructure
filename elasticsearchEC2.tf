615d05c781a9ee67eab85f568489de3f6329b2b7
elvia-si <elvia.sicuro@cloudreach.com>
2022-01-13 16:19:21 +0100

    ec2 test

--- elasticsearchEC2.tf
+++ elasticsearchEC2.tf
@@ -1,19 +1,17 @@
 module "network" {
-  source = "/vpc"
-  name = "my_vpc"
-  vpc_cidr         = "10.10.0.0/16"
-  region           = "eu-west-1"
-  vpc_name         = "ELK-Stack-VPC"
-  internet_gw_name = "team1-new-INT-GW"
-  public_cidr_a    = "10.10.1.0/24"
-  private_cidr_a = "10.10.2.0/24"
-  private_cidr_b = "10.10.3.0/24"
-  enable_nat_gateway  = true
-  single_nat_gateway = true
+  source             = "./vpc"
+  public_name               = "my_vpc"
+  vpc_cidr           = "10.10.0.0/16"
+  region             = "eu-west-1"
+  vpc_name           = "ELK-Stack-VPC"
+  internet_gw_name   = "team1-new-INT-GW"
+  public_cidr_a      = "10.10.1.0/24"
+  private_cidr_a     = "10.10.2.0/24"
+  private_cidr_b     = "10.10.3.0/24"
 }
 
 resource "aws_security_group" "private_elasticsearch_sg" {
- vpc_id      = module.network.my_vpc_id
+  vpc_id = module.network.my_vpc_id
 
   # INBOUND RULES
   ingress {
@@ -35,15 +33,15 @@ resource "aws_security_group" "private_elasticsearch_sg" {
 
   ingress {
     description = "elasticsearch port"
-    from_port = 9200
-    to_port = 9300
-    protocol = "tcp"
-    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port   = 9200
+    to_port     = 9300
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   #OUTBOUND RULES
 
-   egress {
+  egress {
     description = "allow HTTP access"
     from_port   = 80
     to_port     = 80
@@ -51,7 +49,7 @@ resource "aws_security_group" "private_elasticsearch_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-    egress {
+  egress {
     description = "allow SSH access"
     from_port   = 22
     to_port     = 22
@@ -76,13 +74,13 @@ resource "aws_security_group" "private_elasticsearch_sg" {
 
 
 resource "aws_instance" "elasticsearch_server" {
-  ami             = "ami-07d8796a2b0f8d29c" 
-  subnet_id       = module.network.private_a_id
-  instance_type   = "t2.medium"
-  key_name        = "Team1KeyPair"
+  ami                    = "ami-07d8796a2b0f8d29c"
+  subnet_id              = module.network.private_a_id
+  instance_type          = "t2.medium"
+  key_name               = "Team1KeyPair"
   vpc_security_group_ids = [aws_security_group.private_elasticsearch_sg.id]
-  tags            = { 
-      Name = "elasticsearch-server" 
-    }
-
+  tags = {
+    Name = "elasticsearch-server"
+  }
+  depends_on = [module.network]
 }
